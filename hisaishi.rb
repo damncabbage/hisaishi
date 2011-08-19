ad2e448f98e048fedf6cc9891b5754ea1a4d6549
Geoffrey Roberts <g.roberts@blackicemedia.com>
2011-08-20 00:52:28 +1000

    Added vote restrictions to song selection

--- hisaishi.rb
+++ hisaishi.rb
@@ -145,6 +145,7 @@ def rand_song
 		'SELECT * FROM songs s ' + 
 		'LEFT JOIN votes v ON (v.song_id = s.id AND v.user="' + session[:username] + '") ' + 
 		'WHERE v.vote_id IS NULL ' + 
+		'AND v.no < 3 AND v.yes < 1 ' + 
 		'ORDER BY RANDOM() LIMIT 1;'
 	)
 end
