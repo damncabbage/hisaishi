0c407b602d6b1deee67bb87371ee030c026d9f3b
pyrmont <mike@inqk.net>
2011-08-30 23:18:43 +0900

    Hides mouse cursor if no activity for 10 seconds.

--- public/effects.js
+++ public/effects.js
@@ -4,3 +4,25 @@ var prepControls = function() {
 		$(this).find('.controlbar-container').slideToggle();
 	});
 };
+
+var hideCursor = function() {
+  var timer = 0, oldX = 0, oldY = 0;
+  
+  var startTimer = function() {
+    timer = setTimeout(function(){
+      $('body').css({cursor: 'none'});
+    }, 10000);
+  };
+  
+  $(document).mousemove(function(e) {
+    newX = e.pageX;
+    newY = e.pageY;
+    if(newX != oldX || newY != oldY) {
+      $('body').css({cursor: 'default'});
+      clearTimeout(timer);
+      startTimer();
+    }
+    oldX = newX;
+    oldY = newY;
+  })
+}
