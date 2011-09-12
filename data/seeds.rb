24c8ba106ccc78216137437d1e713c716fca59df
Rob Howard <damncabbage@gmail.com>
2011-09-12 23:15:10 +1000

    seeds.rb now maps CSV column headers to song fields.
    Previously, each column was mapped to a field by row[1],
    row[2], etc; now seeds.rb uses the column header to
    pick which field to map to.
    
    tl;dr CSVs are now magic.

--- data/seeds.rb
+++ data/seeds.rb
@@ -2,25 +2,17 @@
 # The data can then be loaded with the rake db:seed
 
 require 'csv'
+require 'pp'
 
-first = true
+seeds_path = File.dirname(__FILE__) + "/seeds.csv"
 
-CSV.foreach(File.dirname(__FILE__) + "/seeds.csv") do |row|
-  unless first
-    song = Song.new
-    song.title =  row[0]
-    song.artist = row[1]
-    song.album = row[2]
-    song.anime = row[3]
-    song.genre = row[4]
-    song.language = row[5]
-    song.karaoke = row[6]
-    song.source_dir = row[7]
-    song.audio_file = row[8]
-    song.lyrics_file = row[9]
-    song.image_file = row[10]
-    song.save!
+CSV.foreach(seeds_path, :headers => :first_row) do |row|
+  song = Song.new
+
+  # Use the column names to set the fields.
+  row.each do |field,value|
+    song.send "#{field}=", value
   end
-  
-  first = false
+
+  song.save!
 end
