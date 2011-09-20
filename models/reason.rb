bd0d893a451444fbafa58d26cc25ed77094a0331
pyrmont <mike@inqk.net>
2011-09-20 15:20:01 +0900

    Added in information about whether this is the karaoke version.

--- models/reason.rb
+++ models/reason.rb
@@ -1,7 +1,7 @@
 class Reason
   include DataMapper::Resource
   property :id,    Serial
-  property :type,      Enum[ :none, :wrong, :mistimed, :misspelt, :details ], :default => :none
+  property :type,      Enum[ :none, :wrong, :mistimed, :misspelt, :details, :other ], :default => :none
   property :comment,  Text
   
   belongs_to :vote
