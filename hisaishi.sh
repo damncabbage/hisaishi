82ef59acff5d3880ffcc9c9d68a2d57dd7a9364c
Rob Howard <rob@robhoward.id.au>
2012-07-08 13:54:50 +1000

    Makes the hisaishi.sh script change to the current directory before kicking off bundler.

--- hisaishi.sh
+++ hisaishi.sh
@@ -4,4 +4,8 @@
 [[ "$ADMIN_PIN" ]]         || export ADMIN_PIN=1234
 [[ "$DEFAULTS_TO_QUEUE" ]] || export DEFAULTS_TO_QUEUE=1
 
+# Change to wherever this script it located.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Fire it up.
 bundle exec ruby hisaishi.rb
