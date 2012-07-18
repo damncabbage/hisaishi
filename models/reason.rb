2aa64bb6eed98b9a6f5032018f837cd84c4a0eb9
Rob Howard <rob@robhoward.id.au>
2012-07-18 18:32:36 +1000

    Cleans up unused files, tidies up templates.

--- models/reason.rb
+++
@@ -1,8 +0,0 @@
-class Reason
-  include DataMapper::Resource
-  property :id,    Serial
-  property :type,      Enum[ :none, :wrong, :mistimed, :misspelt, :details, :other ], :default => :none
-  property :comment,  Text
-  
-  belongs_to :vote
-end
--- models/vote.rb
+++
@@ -1,9 +0,0 @@
-class Vote
-  include DataMapper::Resource
-  property :vote_id,    Serial
-  property :user,      String
-  property :vote,      Enum[ :yes, :no, :unknown ], :default => :unknown
-  property :song_id,    Integer
-  
-  has n, :reasons
-end
