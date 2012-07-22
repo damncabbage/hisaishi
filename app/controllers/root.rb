7ff2c11810f0867d151ee58ac0adba955342823b
Rob Howard <rob@robhoward.id.au>
2012-07-22 13:20:10 +1000

    Adds /socket route, modified to use SocketPool.

--- app/controllers/root.rb
+++ app/controllers/root.rb
@@ -8,4 +8,31 @@ Hisaishi.controllers do
     render 'root/index'
   end
 
+  # /socket
+  get :socket do
+    return redirect(url(:index)) if !request.websocket?
+
+    hi_json  = {type: 'hi'}.to_json
+    bye_json = {type: 'bye'}.to_json
+
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send(hi_json)
+        settings.socket_pool.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick do
+          # Spam the message back out to all connected clients, player and controller alike.
+          settings.socket_pool.sockets.each do |s|
+            s.send(msg)
+          end
+        end
+      end
+      ws.onclose do
+        ws.send(bye_json)
+        settings.socket_pool.sockets.delete(ws)
+      end
+    end
+  end
+
 end
